748eb9364fb11911e85499c1235288ce121beeec
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-06-16 14:57:25 +0200

    add basic book constructor and example from exercise

---
+++ app.js
@@ -0,0 +1,15 @@
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  if (read) {
+    this.read = "already read";
+  } else {
+    this.read = "not read yet";
+  }
+  this.info = function () {
+    return `${this.name} by ${this.author}, ${pages} pages, ${this.read}`;
+  };
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);

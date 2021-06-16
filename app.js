a2eb0083ea72ef6f89192815c4c2ef214e96194a
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-06-16 16:06:49 +0200

    add displayBooks function
    first try to update the display after adding a new book to the array

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Variable declarations.
+const container = document.querySelector("#container");
 const addBookBtn = document.querySelector("#add-book");
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
@@ -13,9 +14,15 @@ const ROTK = new Book("The Return of the King", "J.R.R. Tolkien", 736, true);
 
 let myLibrary = [theHobbit, FotR, TT, ROTK];
 
+displayBooks();
+
 // Event Listeners.
 addBookBtn.addEventListener("click", () => {
   myLibrary.push(addBookToLibrary());
+  for (let i = 0; i < myLibrary.length - 1; i++) {
+    container.removeChild(bookContainer);
+  }
+  displayBooks();
 });
 
 // Functions.
@@ -40,4 +47,29 @@ function addBookToLibrary() {
   let read = prompt("Have you read it? (true or false)", "");
   const newBook = new Book(title, author, pages, read);
   return newBook;
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const bookContainer = document.createElement("div");
+    const bookTitle = document.createElement("h2");
+    bookTitle.textContent = book.title;
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = book.author;
+    const bookPages = document.createElement("p");
+    bookPages.textContent = book.pages;
+    const bookRead = document.createElement("p");
+    bookRead.textContent = book.read;
+
+    bookContainer.style.border = "2px solid black";
+    bookContainer.style.margin = "2vh";
+    bookContainer.style.padding = "20px";
+    bookContainer.style.width = "200px";
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookPages);
+    bookContainer.appendChild(bookRead);
+    container.appendChild(bookContainer);
+  });
 }

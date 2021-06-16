d63a0582756fcb29d8a22208c5f5e341b8f01e8e
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-06-16 15:11:36 +0200

    create book objetcs to populate array and change property name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-let myLibrary = [];
+let myLibrary = [theHobbit, FotR, TT, ROTK];
 
-function Book(name, author, pages, read) {
-  this.name = name;
+function Book(title, author, pages, read) {
+  this.title = title;
   this.author = author;
   this.pages = pages;
   if (read) {
@@ -10,8 +10,24 @@ function Book(name, author, pages, read) {
     this.read = "not read yet";
   }
   this.info = function () {
-    return `${this.name} by ${this.author}, ${pages} pages, ${this.read}`;
+    return `${this.name} by ${this.author}, ${this.pages} pages, ${this.read}`;
   };
 }
 
+function addBookToLibrary() {
+  let title = prompt("Write the book's title: ", "");
+  let author = prompt("Write the author's name: ", "");
+  let pages = prompt("How many pages?", "");
+  let read = prompt("Have you read it? (true or false)", "");
+  return new Book(title, author, pages, read);
+}
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+const FotR = new Book(
+  "The Fellowship of the Ring",
+  "J.R.R. Tolkien",
+  736,
+  true
+);
+const TT = new Book("The Two Towers", "J.R.R. Tolkien", 608, true);
+const ROTK = new Book("The Return of the King", "J.R.R. Tolkien", 736, true);
